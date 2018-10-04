69d0e6cd82bb08c8c0278532cf758d43485ebd24
Stanisław Romek <Stanisaw Romek>
2018-10-04 22:59:19 +0200

    Aktualizacja szkieletu formularza

--- js/script.js
+++ js/script.js
@@ -1,8 +1,40 @@
+$(".form-client").hide();
 $(".add-new-servis-hide").hide();
 
-$(document).ready(function(){       
+var formDate, formNumberCar, formTypeService, formWorker, formTime;
+var testy;
+
+
+$(document).ready(function(){    
+    
+    $(".btn-start").click(function(){        
+        $(".form-client").toggle();        
+    });         
+    
     $("#add-service").click(function(){        
         $("#add-new-servis-input").toggle();
         $("#add-new-servis-button").toggle();                
     });         
+    
+    
+    $("#add-new-service-btn").click(function(){        
+        $(".add-new-servis-hide").hide();        
+    });         
+    
+    var testy = $("#btn-add-record").click(function(){        
+        var formDate = $("#date-input").val();        
+        var formNumberCar = $("#number-car").val();
+        var formTypeService = $("#service-car").val();
+        var formWorker = $("#worker-car").val();
+        var formTime = $("#time-service-car").val();
+//        return console.log(formDate + ", " + formNumberCar + ", " + formTypeService)
+        return console.log(formDate + ", " + formTypeService + ", " + formWorker + ", " + formTime);
+        
+    });         
+    
+    console.log(testy);
+    
+    
+    
+    
 });
