11eceb0cd32b8b6c64a2f333f9f201887a1e22fd
Stanisław Romek <Stanisaw Romek>
2018-10-05 18:56:23 +0200

    Dodanie przycisku i akcji pod elementy formularza

--- js/script.js
+++ js/script.js
@@ -1,40 +1,77 @@
-$(".form-client").hide();
-$(".add-new-servis-hide").hide();
+var formDate, formNumberCar, formTypeService, formWorker, formTime, formNewService;
 
-var formDate, formNumberCar, formTypeService, formWorker, formTime;
-var testy;
+var main = function(){
+    formHide();
+    newServiceHide();
+};
 
+//    Ukrywanie formularza przy uruchomieniu strony
+var formHide = function() {
+    $(".form-client").hide(); 
+};
 
-$(document).ready(function(){    
-    
+//    Odkryci formularza po liknięciu przycisku DODAJ ZLECENE
     $(".btn-start").click(function(){        
         $(".form-client").toggle();        
-    });         
-    
+    }); 
+
+//    Ukrywanie pola do dodania nowej usługi po kliknięciu przycisku DODAJ ZLECENIE
+var newServiceHide = function() {
+    $(".add-new-servis-hide").hide();
+}
+
+//    Odkrycie pola input i przycisku dodaj do wstawiania nowych rodzajów usług po kliknięciu przycisku DOdaj nową usługę do bazy
     $("#add-service").click(function(){        
-        $("#add-new-servis-input").toggle();
-        $("#add-new-servis-button").toggle();                
-    });         
-    
-    
-    $("#add-new-service-btn").click(function(){        
-        $(".add-new-servis-hide").hide();        
-    });         
-    
-    var testy = $("#btn-add-record").click(function(){        
-        var formDate = $("#date-input").val();        
-        var formNumberCar = $("#number-car").val();
-        var formTypeService = $("#service-car").val();
-        var formWorker = $("#worker-car").val();
-        var formTime = $("#time-service-car").val();
-//        return console.log(formDate + ", " + formNumberCar + ", " + formTypeService)
-        return console.log(formDate + ", " + formTypeService + ", " + formWorker + ", " + formTime);
+        $(".add-new-servis-hide").toggle();
+    });   
+
+//    Dodanie nowej usługi do lsty wszystkich rodzajów usług    
+    $("#add-new-service-btn").click(function(){
+//      1. Pobieramy wartość inputa i wstawiamy do zmiennej formNewService
+        var formNewService = $(".new-device-input").val();         
         
-    });         
+    });
     
-    console.log(testy);
+
+
+
+
+
+
+
+
+
+
+
+//
+//var formTypeService = $("#service-car").val();
+//
+//    $("#btn-add-record").click(function(){        
+//        $("#pole-testowe").append("formTypeService.val()");
+//    });
+
+
+
+   
+
+
+//$(document).ready(function(){    
     
-    
-    
-    
-});
+//    $("#add-new-service-btn").click(function(){        
+//        $(".add-new-servis-hide").hide();        
+//    });         
+//    
+//    var testy = $("#btn-add-record").click(function(event){        
+//        var formDate = $("#date-input").val();        
+//        var formNumberCar = $("#number-car").val();
+//        var formTypeService = $("#service-car").val();
+//        var formWorker = $("#worker-car").val();
+//        var formTime = $("#time-service-car").val();
+//        return formDate;       
+//    });         
+   
+            
+//});
+
+
+document.addEventListener('DOMContentLoaded', main);
